7d70996f390a023457a745f40e5b33f9a0943d38
Sam Laubscher <hakaewt@protonmail.com>
2021-01-17 21:49:06 +0000

    Correct AWS secret access key typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,7 +177,7 @@ if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-retail'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
